ac2678d411d20b50a2aa42f1e8e942209d4ac4a9
Roshan Jossy <roshanjossey@gmail.com>
2022-10-31 01:27:05 +0100

    install and configure relay

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  compiler: {
+    relay: {
+      // This should match relay.config.js
+      src: './',
+      artifactDirectory: './__generated__',
+      language: 'typescript',
+    },
+  },
 }
 
 module.exports = nextConfig
